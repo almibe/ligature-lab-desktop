9e84f6b63f5bcb4094be54df03ea77db4e429693
Alex Michael Berry <berry3@indiana.edu>
2019-10-09 23:50:38 -0400

    last commit before javafx rewrite

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm").version("1.3.21")
+    id("org.jetbrains.kotlin.jvm").version("1.3.50")
     application
 }
 
@@ -14,6 +14,10 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     compile("org.almibe:ligature:0.1.0-SNAPSHOT")
+    compile("org.almibe:multi-page-display:0.4.0-SNAPSHOT")
+    implementation("org.koin:koin-core:2.0.1")
+    implementation("org.koin:koin-core-ext:2.0.1")
+    testImplementation("org.koin:koin-test:2.0.1")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
