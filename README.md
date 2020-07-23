2306efe5dd92aa3ca2199b6a62c4b322b4e9ea60
Alex Michael Berry <berry3@indiana.edu>
2020-07-22 20:51:22 -0400

    adjusting name from ligature-lab-local to ligature-lab-desktop

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ligature-lab-local
-A version of ligatureLab that is intended to be used from a local machine.
+# ligature-lab-desktop
+A version of ligatureLab that is intended to be used from a local desktop machine.
--- src/test/scala/dev/ligature/lab/desktop/Spec.scala
+++ src/test/scala/dev/ligature/lab/desktop/Spec.scala
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package dev.ligature.lab.local
+package dev.ligature.lab.desktop
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
