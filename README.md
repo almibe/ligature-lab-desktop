06225b49355852042d487a1f87c96ce781f3dbf0
Alex Michael Berry <berry3@indiana.edu>
2020-07-22 20:47:52 -0400

    adding basic project structure + renaming to ligature-lab-local, closes #12

---
+++ README.md
@@ -0,0 +1,2 @@
+# ligature-lab-local
+A version of ligatureLab that is intended to be used from a local machine.
---
+++ src/test/scala/dev/ligature/lab/local/Spec.scala
@@ -0,0 +1,14 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package dev.ligature.lab.local
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class Spec extends AnyFlatSpec with Matchers {
+  it should "blah" in {
+    1 + 1 shouldBe 2
+  }
+}
